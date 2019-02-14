449888870a0d94ccd81a97ea4d228e1c7790aec0
shaoyingzhu <yingzhu.shao@newtouch.cn>
2019-02-14 21:40:04 +0800

    初始化spring项目为springboot

---
+++ src/main/java/com/syz/springBoot/comfig/Application.java
@@ -0,0 +1,11 @@
+package com.syz.springBoot.comfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/*第三步创建启动类*/
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
